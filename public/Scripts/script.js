bdf61c646b84c62ba53867e5db62f173d76bda71
anshole <anshzinc@gmail.com>
2015-01-10 03:03:12 +0530

    Make tables editable

--- public/Scripts/script.js
+++ public/Scripts/script.js
@@ -1,3 +1,24 @@
-window.onload = function () {
+$(function() {
+	$("td").dblclick(function(e) {
+		if ($(this).children().first().is(":focus")) {
+			return false;
+		} else {
+			var prevVal = $(this).text();
+			$(this).html("<input type='text' value='" + prevVal + "' />");
+			$(this).children().first().focus();
 
-}
+			$(this).children().first().keyup(function(e) {
+				if (e.keyCode == 27) { // Escape
+					$(this).parent().text(prevVal);
+				} else if (e.keyCode == 13) { // Enter
+					var newVal = $(this).val();
+					$(this).parent().text(newVal);
+				}
+			});
+		}
+
+		$(this).children().first().blur(function() {
+			$(this).parent().text(prevVal);
+		});
+	});
+});
