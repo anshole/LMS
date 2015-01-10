40fb9a84ac3b0fe0ee5c8d86b6f831400e145cdb
anshole <anshzinc@gmail.com>
2015-01-10 06:06:15 +0530

    Create Sheet_info, Sheet_cells models

---
+++ app/models/Sheet_cells.php
@@ -0,0 +1,29 @@
+<?php
+
+use Illuminate\Auth\UserTrait;
+use Illuminate\Auth\UserInterface;
+use Illuminate\Auth\Reminders\RemindableTrait;
+use Illuminate\Auth\Reminders\RemindableInterface;
+
+class Sheet_cells extends Eloquent implements UserInterface, RemindableInterface {
+	
+	protected $fillable = array('cell_id', 'sheet_id', 'cell_row', 'cell_column', 'cell_content');
+
+	use UserTrait, RemindableTrait;
+
+	/**
+	 * The database table used by the model.
+	 *
+	 * @var string
+	 */
+	protected $table = 'sheet_info';
+
+	/**
+	 * The attributes excluded from the model's JSON form.
+	 *
+	 * @var array
+	 */
+	//protected $hidden = array('password', 'remember_token');
+
+}
+
---
+++ app/models/Sheet_info.php
@@ -0,0 +1,29 @@
+<?php
+
+use Illuminate\Auth\UserTrait;
+use Illuminate\Auth\UserInterface;
+use Illuminate\Auth\Reminders\RemindableTrait;
+use Illuminate\Auth\Reminders\RemindableInterface;
+
+class Sheet_info extends Eloquent implements UserInterface, RemindableInterface {
+	
+	protected $fillable = array('sheet_id', 'sheet_name', 'sheet_creator');
+
+	use UserTrait, RemindableTrait;
+
+	/**
+	 * The database table used by the model.
+	 *
+	 * @var string
+	 */
+	protected $table = 'sheet_info';
+
+	/**
+	 * The attributes excluded from the model's JSON form.
+	 *
+	 * @var array
+	 */
+	//protected $hidden = array('password', 'remember_token');
+
+}
+
